ad72d731a66c1b2716f9054071b3c56da5eb8853
Metr_yumora <metryumora@gmail.com>
2017-06-12 10:05:27 +0300

    Initial commit

---
+++ src/main/java/com/chdtu/fitis/dipladd/DiplomaAdditionApplication.java
@@ -0,0 +1,12 @@
+package com.chdtu.fitis.dipladd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaAdditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaAdditionApplication.class, args);
+	}
+}
